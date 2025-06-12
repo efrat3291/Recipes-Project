8d145c32f17bac6647674617b47d2a23babd1783
Efrat_Goldberg <efrat79071@gmail.com>
2025-06-12 13:04:00 +0300

    add user routes

---
+++ routes/user.router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { getAllUsers, signIn, signUp, deleteUser, updateUser } from "../controllers/user.controller.js";
+import { checkAuth, checkIsAdmin } from "../middlewares/auth.middleware.js";
+import { checkPreferences } from "joi";
+
+const router = Router();
+
+router.get('/', checkAuth, checkIsAdmin, getAllUsers);
+router.post('/sign-in', signIn);
+router.post('/', signUp);
+router.put('/:id', checkAuth, newPassword);
+router.delete('/id', checkAuth, checkIsAdmin, deleteUser);
+
+export default router;
