2f35d477646dd59e8dc5068f15a2f25ebc7c3a14
Efrat_Goldberg <efrat79071@gmail.com>
2025-06-12 14:18:00 +0300

    coonect to mongo Atlas

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+    const DB_URI = proccess.env.DB_URI || 'mongodb://127.0.0.1/recipesDB'
+    try{
+        await mongoose.connect(DB_URI);
+    }catch(error){
+        console.log(`Error connecting to DB: ${error.message}`);
+        process.exit(1);
+    }
+}
