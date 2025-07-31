1dc660f70f6a42fde6c82c12c57c4752435bf3dc
Efrat_Goldberg <efrat79071@gmail.com>
2025-07-31 15:32:27 +0300

    update in category controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -8,22 +8,22 @@ export const getAllCategories = async (req, res, next) => {
         next({message: error.message});
     }
 }
-
-export const getAllCategoriesAndRecipes = async (req, res, next) => {
-    try{
-        const categories = await Category.find().populate('recipes');
-        res.status(200).json(categories);
-    }catch(error){
-        next({message: error.message});
+export const getAllRecipesByCategory = async (req, res, next) => {
+    try {
+        const recipesByCategory = await Category.find.populate('recipes');
+        res.status(200).json(recipesByCategory);
+    } catch (error) {
+        next({ message: error.message });
     }
-}
+};
 
-export const getCategoryById = async (req, res, next) => {
-    try{
-        const {_id} = req.params;
-        const category = await Category.findById(_id).populate('recipes');
-        res.status(200).json(category);
-    }catch(error){
-        next({message: error.message});
+export const getRecipesByCategoryId = async (req, res, next) => {
+    try {
+        const { _id } = req;
+        const recipesByCategory = await Category.findById(_id).populate('recipes');
+        res.status(200).json(recipesByCategory);
+    } catch (error) {
+        next({ message: error.message });
     }
-}
+};
+
