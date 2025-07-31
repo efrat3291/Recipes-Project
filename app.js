beeb62df98001331ad4149b2e58469ec784c3565
Efrat_Goldberg <efrat79071@gmail.com>
2025-07-31 19:32:15 +0300

    update

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 import express from 'express';
-import cors from 'cors';
+import userRouter from './routes/user.router.js';
+import recipeRouter from './routes/recipes.router.js';
 import { config } from 'dotenv';
 import { connectDB } from './config/db.js';
-import userRouter from './routes/user.router.js';
 import morgan from 'morgan';
-import { errorHandler } from './middlewares/error.middleware.js';   
-
+import cors from 'cors';
+import { notFound,errorHandler } from './middlewares/errorHandling.middleware.js';
 
 config();
-
 connectDB();
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+const app=express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 app.use(cors());
-
-app.use('/users', userRouter);
-
-app.use(notFound, errorHandler);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use('/users',userRouter);
+app.use('/recipes',recipeRouter);
+app.use('/categories',recipeRouter);
+app.put('/try/:id',(req,res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
+    console.log(req.headers);
+    res.send();   
 });
-    
-    
-    
-    
+app.use(notFound);
+app.use(errorHandler);
+const port=process.env.PORT||3000;
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);   
+})
